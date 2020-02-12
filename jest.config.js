dc1b2ad474b7b9df788140002cc4b12f056b0ca3
Corbifex <sander@moosty.com>
2020-02-12 14:34:50 +0100

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  collectCoverage: true,
+  reporters: [
+    "default",
+  ],
+  coverageReporters: [
+    'text',
+    'html',
+    'lcov',
+    'cobertura',
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
